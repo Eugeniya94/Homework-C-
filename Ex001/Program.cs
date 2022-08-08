02cc3ea1790d58dc7aa141cc6ad9ac7fd4f22a56
Evgeniya <sluchevskaya0309@yandex.ru>
2022-08-08 18:18:11 +0300

    add second version in Ex001

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -2,6 +2,10 @@
 // принимает два числа и выдаёт, какое число большее, а
 // какое меньшее.
 
+
+// Сделала 2 варианта, проверьте пожалуйста оба, можно ли так писать.
+
+
 Console.Write("Введите первое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
@@ -19,3 +23,16 @@ else {
 
 
 
+
+/*Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2) {
+    Console.WriteLine($"Число {num1} больше чем {num2}");
+}
+else {
+     Console.WriteLine($"Число {num2} больше чем число {num1}");
+}
+*/
