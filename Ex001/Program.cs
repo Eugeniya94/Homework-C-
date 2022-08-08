e59bd579ef444ebc9c886d0aebd111b48e24a2da
Evgeniya <sluchevskaya0309@yandex.ru>
2022-08-08 18:04:32 +0300

    add changes in Ex001

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход
+// принимает два числа и выдаёт, какое число большее, а
+// какое меньшее.
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2) {
+    Console.WriteLine($"max = {num1}");
+    Console.WriteLine($"min = {num2}");
+}
+else {
+    Console.WriteLine($"max = {num2}");
+    Console.WriteLine($"min = {num1}");
+}
+
+
+
+
