068c9843504cb3387ff87aef986fba47f52c761b
Evgeniya <sluchevskaya0309@yandex.ru>
2022-08-09 11:58:53 +0300

    Change Ex003

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -3,3 +3,13 @@
 // (делится ли оно на два без остатка).
 
 
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 0){
+    Console.WriteLine($"Число {number} является четным");
+
+}
+else {
+    Console.WriteLine($"Число {number} НЕ является четным");
+}
