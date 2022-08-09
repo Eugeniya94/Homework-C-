004d277d15d51a1615764dd2da73f5020913b0eb
Evgeniya <sluchevskaya0309@yandex.ru>
2022-08-09 12:46:30 +0300

    Change Ex004

--- Ex004/Program.cs
+++ Ex004/Program.cs
@@ -3,3 +3,17 @@
 // числа от 1 до N.
 
 
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+Console.Write($"Все четные числа от 1 до {number}: ");
+
+while (i <= number) {
+    if (i % 2 == 0)
+        Console.Write(i + ",");
+        i++;
+    
+}
+
+
